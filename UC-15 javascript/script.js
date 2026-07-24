9dee05f938c9ff333e4df77541a147c41b602b17
samuel3006am-arch <samuel3006am@gmail.com>
2026-07-24 18:53:29 -0300

    Add files via upload

---
+++ UC-15 javascript/script.js
@@ -0,0 +1,15 @@
+console.log("Olá,mundo!")
+
+confirm("Você deseja continuar?")
+prompt("Qual o seu nome?")
+alert("Boas vindas")
+console.log("A cidade do usuário é natal")
+
+let numero = prompt("Qual o numero")
+console.log("numero" + 67)
+
+if (numero !== "7") {
+    alert("parabens")
+} else {
+    alert("reprovado")
+}
