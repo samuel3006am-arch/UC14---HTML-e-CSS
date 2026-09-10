7464b98efd5b07d82f2ee346825de6f8f744cfa6
samuel3006am-arch <samuel3006am@gmail.com>
2026-09-10 17:55:46 -0300

    Add files via upload

---
+++ js liquid glass/script.js
@@ -0,0 +1,48 @@
+console.log("Ola, Luiz! seja bem vindo");
+console.log("Ola, Gustavo! Seja bem vindo");
+console.log("Ola, Cecilia! Seja bem vinda");
+
+function darBoasvindas(name) {
+    console.log(`Ola,${nome}! Seja be-vindo!`);
+}
+
+darBoasvindas("Luiz");
+darBoasvindas("Gustavo");
+darBoasvindas("Cecilia");
+
+function apresentar(nome, idade) {
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+}
+
+
+apresentar("Gaby",24)
+
+
+//Criar uma função que recebe o nome de uma pessoa e mostra uma mensagem dizendo se ela está estudando.
+
+function estaEstudando(nome, materia) {
+    console.log(`Meu nome é ${nome} e estudo ${materia} anos.`);
+}
+
+apresentar("Samuel","Fisica")
+
+function somar(a,b) {
+  return a + b;
+}
+
+somar(5, 3)
+
+
+let resultado = somar(5, 3);
+
+console.log(resultado);
+
+
+
+function somar(7, 6) {
+  return 7 + 6;
+}
+
+let resultado = somar(7, 6);
+
+console.log(resultado);
