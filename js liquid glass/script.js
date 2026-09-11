309ff1ca86a2b37a06424d93f41c9572c08a0e08
samuel3006am-arch <samuel3006am@gmail.com>
2026-09-11 17:40:06 -0300

    Add files via upload

--- js liquid glass/script.js
+++ js liquid glass/script.js
@@ -46,3 +46,50 @@ function somar(7, 6) {
 let resultado = somar(7, 6);
 
 console.log(resultado);
+
+
+function calcularMedia(nota1, nota2) {
+  return (nota1 + nota2) / 2;
+}
+
+let nome = prompt("Digite o nome do aluno:");
+let nota1 = Number(prompt("Digite a primeira nota:"));
+let nota2 = Number(prompt("Digite a segunda nota:"));
+
+let media1 = calcularMedia(nota1, nota2);
+
+console.log(`${nome} ficou com média ${media1}`);
+
+if(media1 >=6) {
+  console.log(`${nome} está aprovado!`);
+} else {
+  console.log(`${nome} está reprovado!`);
+}
+
+calcularMedia(nota1, nota2)
+
+
+
+function calcularCustoViagem(passagem, hospedagem, alimentacao, passeios) {
+  return passagem + hospedagem + alimentacao + passeios;
+}
+
+
+let destino = prompt("Digite o destino da viagem:");
+let passagem = Number(prompt("Digite o valor da passagem:"));
+let hospedagem = Number(prompt("Digite o valor da hospedagem:"));
+let alimentacao = Number(prompt("Digite o valor da alimentação:"));
+let passeios = Number(prompt("Digite o valor dos passeios:"));
+
+
+let custoTotal = calcularCustoViagem(passagem, hospedagem, alimentacao, passeios);
+
+// Exibindo o custo total formatado
+console.log(`A viagem para ${destino} ficou com o custo total de R$ ${custoTotal.toFixed(2)}`);
+
+
+if (custoTotal <= 2000) {
+  console.log("A viagem está DENTRO do orçamento!");
+} else {
+  console.log("A viagem está FORA do orçamento!");
+}
