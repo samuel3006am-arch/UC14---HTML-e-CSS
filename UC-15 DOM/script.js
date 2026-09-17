d9001d4fdf9ae251681ef7dc15108f9d750b4eda
samuel3006am-arch <samuel3006am@gmail.com>
2026-09-17 17:43:47 -0300

    Add files via upload

---
+++ UC-15 DOM/script.js
@@ -0,0 +1,8 @@
+function mudarTexto() {
+    document.getElementById("titulo").
+    textContent = "Você clicou no botão!"
+}
+
+function mudarCor() {
+    document.getElementById("mensagem").style.color = "blue";
+}
